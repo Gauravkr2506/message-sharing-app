4fbf2ce6bde1bc01dc08eee61a3a8654da569326
Healthmug <healthmug@Healthmugs-iMac.local>
2019-01-09 15:29:45 +0530

    add modal

--- Awesome/App.js
+++ Awesome/App.js
@@ -2,23 +2,27 @@ import SplashScreen from "react-native-splash-screen";
 import { AdMobBanner } from "react-native-admob";
 import { data } from "./data";
 import React, { Component } from "react";
-import { StyleSheet, Text, View, FlatList, Share, TouchableOpacity, ImageBackground } from "react-native";
+import { StyleSheet, Text, View, FlatList, Share, TouchableOpacity, ImageBackground, Modal, TouchableHighlight, Alert, ScrollView, Image } from "react-native";
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.share = this.share.bind(this);
 		this.getData = this.getData.bind(this);
+		this.state = { modalVisible: false, msg: {} };
 	}
 	componentDidMount() {
 		SplashScreen.hide();
 	}
 	share(data) {
-		Share.share({
-			title: data.title,
-			message: data.message
-		});
+		this.setState({ modalVisible: true, msg: data });
+
+		// Share.share({
+		// 	title: data.title,
+		// 	message: data.message
+		// });
 	}
+
 	_renderItem = ({ item }) => <MyListItem share={data => this.share(data)} data={item} />;
 	getData() {
 		let list_data = [];
@@ -54,6 +58,46 @@ export default class App extends Component {
 				<View style={{ flex: 1, backgroundColor: "#008000" }}>
 					<AdMobBanner adSize="smartBannerPortrait" adUnitID="ca-app-pub-9969212413329273/2210166532" />
 				</View>
+				<Modal
+					animationType="slide"
+					transparent={false}
+					visible={this.state.modalVisible}
+					onRequestClose={() => {
+						this.setState({ modalVisible: false });
+						Alert.alert("Modal has been closed.");
+					}}
+				>
+					<View style={{ flex: 1 }}>
+						<View style={{ height: 50, padding: 10 }}>
+							<Text
+								style={{ fontSize: 16 }}
+								onPress={() => {
+									this.setState({ modalVisible: false });
+								}}
+							>
+								Back
+							</Text>
+						</View>
+
+						<ScrollView contentContainerStyle={{ flex: 1 }}>
+							<Text style={{ fontSize: 16, textAlign: "center" }}>{this.state.msg.message}</Text>
+						</ScrollView>
+
+						<View style={{ flex: 3, backgroundColor: "green" }}>
+							<View style={{ flex: 2, flexDirection: "row", justifyContent: "space-around", backgroundColor: "white" }}>
+								<TouchableHighlight style={{ width: 50, marginTop: 20, height: 50, backgroundColor: "red", justifyContent: "center", alignItems: "center", borderRadius: 25 }}>
+									<Image style={{ width: 20, height: 20 }} source={require("./img/copy.png")} />
+								</TouchableHighlight>
+								<TouchableHighlight style={{ width: 50, marginTop: 20, height: 50, backgroundColor: "red", justifyContent: "center", alignItems: "center", borderRadius: 25 }}>
+									<Image style={{ width: 20, height: 20 }} source={require("./img/share.png")} />
+								</TouchableHighlight>
+							</View>
+							<View style={{ height: 50, backgroundColor: "#333" }}>
+								<Text>adds</Text>
+							</View>
+						</View>
+					</View>
+				</Modal>
 			</ImageBackground>
 		);
 	}
